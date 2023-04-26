74958ad4156540bf605ef748a24985a1170837df
Artur A Pereira <121524915+ArturP28@users.noreply.github.com>
2023-04-25 21:24:28 -0300

    Profile

---
+++ Projeto34/Main34.js
@@ -0,0 +1,30 @@
+function chamar() {
+
+    let imagem = document.createElement('img')
+
+    imagem.setAttribute('id', 'foto')
+
+    imagem.setAttribute('src', 'img-perfil.png')
+
+    document.getElementById('result').appendChild(imagem)
+    
+
+    let nome = document.createElement('p')
+    let idade = document.createElement('p')
+    let area = document.createElement('p')
+
+    
+
+    nome.textContent = 'Nome: Artur Antunes Pereira'
+    idade.textContent = 'Idade: 31 Anos'
+    area.textContent = 'Área: Desenvolvedor Frontend'
+    
+
+    document.getElementById('result').appendChild(nome)
+    document.getElementById('result').appendChild(idade)
+    document.getElementById('result').appendChild(area)
+
+}
+
+
+
